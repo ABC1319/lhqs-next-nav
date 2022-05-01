3c174bebd0ea73423f9823b59a70e99a33aed32a
lhqs <lhqs.gu@gmail.com>
2022-05-01 12:53:14 -0400

    feat: add 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,12 @@
+import Layout from "../components/Layout"
+
 export default function CustomNotFound() {
-    return <div className="text-center text-xl">404</div>
+    return <Layout>
+        <div
+            class="p-4 text-green-700 border rounded border-green-900/10 bg-green-50"
+            role="alert"
+            >
+            <strong class="text-sm font-medium"> Page is not found </strong>
+            </div>
+    </Layout>
 }
