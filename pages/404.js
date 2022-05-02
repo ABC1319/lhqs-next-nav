bbdcfe22c58f64f8d35a8e536b3156d880ac5811
lhqs <lhqs.gu@gmail.com>
2022-05-01 20:03:50 -0400

    fix:404 page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,19 @@
 import Layout from "../components/Layout"
-
+import Link from "next/link"
 export default function CustomNotFound() {
     return <Layout>
-        <div
-            class="p-4 text-green-700 border rounded border-green-900/10 bg-green-50"
-            role="alert"
-            >
-            <strong class="text-sm font-medium"> Page is not found </strong>
+            <div className="relative overflow-hidden bg-white">
+                <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+                    <div className="w-full font-mono flex flex-col items-center relative z-10">
+                        <h1 className="text-black font-extrabold text-5xl text-center text-white leading-tight mt-4">Page is not found</h1>
+                        <p className="text-black font-extrabold text-8xl my-44 text-white animate-bounce">
+                            404
+                        </p>
+                        <Link href="/"><a className="underline">Home</a></Link>
+                    </div>
+                </div>
             </div>
+
+
     </Layout>
 }
