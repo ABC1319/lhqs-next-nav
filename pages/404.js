82abe17084bd5c66971122d76e2d0367315de99f
lhqs <lhqs.gu@gmail.com>
2022-05-01 20:07:31 -0400

    fix:404 page

--- pages/404.js
+++ pages/404.js
@@ -5,8 +5,8 @@ export default function CustomNotFound() {
             <div className="relative overflow-hidden bg-white">
                 <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
                     <div className="w-full font-mono flex flex-col items-center relative z-10">
-                        <h1 className="text-black font-extrabold text-5xl text-center text-white leading-tight mt-4">Page is not found</h1>
-                        <p className="text-black font-extrabold text-8xl my-44 text-white animate-bounce">
+                        <h1 className="text-black font-extrabold text-5xl text-center leading-tight mt-4">Page is not found</h1>
+                        <p className="text-black font-extrabold text-8xl my-44 animate-bounce">
                             404
                         </p>
                         <Link href="/"><a className="underline">Home</a></Link>
