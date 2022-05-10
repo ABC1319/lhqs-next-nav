930d73e47633d2e0c17947fc25695b9f9c022615
lhqs <lhqs.gu@gmail.com>
2022-05-10 10:01:23 -0400

    fix: next link

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
                 <div className="px-4 py-2">
                     <span  className="hover:text-sky-400"><Link href="/hunt" rel="noreferrer"><a>聚合搜索</a></Link></span>
                     <span  className="px-4 hover:text-sky-400"><a href="https://github.com/lhqs/lhqs-next-nav" target="_blank" rel="noreferrer">Github</a></span>
-                    <span id="slogan" className="hover:text-sky-400"><a href="/about" rel="noreferrer">关于</a></span>
+                    <span id="slogan" className="hover:text-sky-400"><Link href="/about" rel="noreferrer"><a>关于</a></Link></span>
                 </div>
             </div>
         </nav>
