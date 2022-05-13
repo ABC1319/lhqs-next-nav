e1e7330879bba19a63bd4494b05099177f9362a5
lhqs <lhqs.gu@gmail.com>
2022-05-13 04:01:19 -0400

    fix: about page class name

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
         {/* <p>Hi, 我的ID是「乱红如雨」, 目前是软件工程师, 在 MindCoord 主要负责后端内容. </p> */}
         <img src="/who.png" alt="我是谁, 我从哪里来, 要到哪里去" width="100%"/>
 
-        <h2 class="pt-4 fo">联系方式: lhqs.gu@gmail.com</h2>
+        <h2 className="pt-4 fo">联系方式: lhqs.gu@gmail.com</h2>
        </div>
     </Layout>
 }
